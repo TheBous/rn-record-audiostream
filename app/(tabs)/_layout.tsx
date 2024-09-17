b392ba8df5102ba6ee6dc54277eb5bc1a9a260b2
TheBous <thebous1993@gmail.com>
2024-09-17 16:53:57 +0200

    feat(sh): logo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,13 +2,14 @@ import { Tabs } from 'expo-router';
 import { KeyRound } from "lucide-react-native";
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Image } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const superHumansSmallLogo = colorScheme === "dark" ? require('../../assets/images/superhumans/superhuman-small-white.png') : require('../../assets/images/superhumans/superhuman-small-black.png');
   return (
     <Tabs
       screenOptions={{
@@ -18,9 +19,10 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'SuperHuman',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            // <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <Image source={superHumansSmallLogo} style={{ width: 24, height: 24 }} alt="Agent avatar" />
           ),
         }}
       />
